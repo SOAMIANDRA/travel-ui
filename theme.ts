5651655af28c6880eba28acc58135c5d13577354
Miaritiana <hei.miaritiana@gmail.com>
2025-06-24 23:54:26 +0300

    feat : configure MUI

---
+++ theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    typography: {
+      fontFamily: 'var(--font-roboto)',
+    },
+  });
+  
+export default theme;
+  
